7c02d97585505138a53df7e52ad3e57d323ed573
shihang <study1228.sh@gmail.com>
2014-03-19 22:03:51 +0800

    修改deploy脚本，复制mysql-connector文件

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 cp -r src/main/webapp/* $CATALINA_HOME/webapps/webservlet/
 cp -r target/classes $CATALINA_HOME/webapps/webservlet/WEB-INF/
+
+mkdir -p $CATALINA_HOME/webapps/webservlet/WEB-INF/lib/ 
+cp -r lib/mysql-connector-java-3.1.14-bin.jar $CATALINA_HOME/webapps/webservlet/WEB-INF/lib/
+
